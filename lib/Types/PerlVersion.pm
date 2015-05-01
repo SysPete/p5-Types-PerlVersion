4f6c38d6cc8c3ef121d573af405fca5b649de547
Peter Mottram <peter@sysnix.com>
2015-05-01 16:43:48 +0200

    cleanup readme files and bump revision for release

--- lib/Types/PerlVersion.pm
+++ lib/Types/PerlVersion.pm
@@ -9,11 +9,11 @@ Types::PerlVersion - L<Perl::Version> type constraint for L<Type::Tiny>
 
 =head1 VERSION
 
-Version 0.001
+Version 0.002
 
 =cut
 
-our $VERSION = '0.001';
+our $VERSION = '0.002';
 
 =head1 SYNOPSIS
 
